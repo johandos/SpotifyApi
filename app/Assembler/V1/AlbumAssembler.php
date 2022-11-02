ddff9ba782d60f42d471513b9cdbb17a3068449a
Johan Ospina <johan.ospina05@gmail.co!>
2022-11-02 23:19:11 +0100

    change image key to cover

--- app/Assembler/V1/AlbumAssembler.php
+++ app/Assembler/V1/AlbumAssembler.php
@@ -12,7 +12,7 @@ class AlbumAssembler
             $results[$key]['name'] = $album->name;
             $results[$key]['release_date'] = $album->release_date;
             $results[$key]['total_tracks'] = $album->total_tracks;
-            $results[$key]['images'] = $album->images;
+            $results[$key]['cover'] = $album->images;
         }
 
         return $results;
